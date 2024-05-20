5ffb8736a524514688066034a6dc50a50d153430
sona <sennansona@gmail.com>
2024-05-20 17:08:27 +0530

    updated

---
+++ fizzbizz.py
@@ -0,0 +1,16 @@
+
+def fizzbizz(n):
+    for i in range(1,n+1):
+        if(i%15==0):
+            print("fizzbizz")
+        elif(i%5==0):
+            print("bizz")
+        elif(i%3==0):
+            print("fizz")
+        else:
+            print(i)
+
+if __name__ == '__main__':
+    n=int(input("enter the  number"))
+    fizzbizz(n)
+    
